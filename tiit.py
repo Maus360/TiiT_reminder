bce7e495af4abfcfbfd93b4ed853e8aaebfc2631
maus <maksimochka1_1@mail.ru>
2016-12-25 22:01:29 +0300

    tiit2

--- tiit.py
+++ tiit.py
@@ -5,45 +5,53 @@ from datetime import datetime
 from text_tiit import text, text_final
 
 api = vk.API(vk.AuthSession(app_id = '5648577', user_login = 'maksim.golosov@mail.ru',
-                            user_password = 'ak-13.ak-13.akm-18.max', scope = 'messages'))
+							user_password = 'ak-13.ak-13.akm-18.max', scope = 'messages'))
 
 
 def message(day, minute, hour):
 	day_x = 28
 	hour_x = 11
 	minute_x = 40
-	
+
 	clock = (minute_x - minute) + 60 * (hour_x - hour) + 24 * 60 * abs(day - day_x)
-	
+
 	d_day = clock // 60 // 24
 	d_hour = clock // 60 % 24
 	d_minute = clock % 60
-	
-	return "До зачета по ТИИТ осталось : {0} с., {1} ч., {2} м.!\n".format(str(d_day), str(d_hour), str(d_minute))
+
+	return "До зачета по ТИИТ осталось : "+str(d_day)+" сут., "+str(d_hour)+" час., "+str(d_minute)+" мин.!\n"
 
 
 def main():
-	(day, minute, hour) = (int(str(datetime.now())[8:10]), int(str(datetime.now())[14:16]),
-	                       int(str(datetime.now())[11:13]))
-	
-	if day == 22:
-		api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
-		time.sleep(4 * 3600)
-		return main()
-	elif day == 27:
-		api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
-		time.sleep(2 * 3600)
-		return main()
-	elif day == 28:
-		if hour >= 9:
-			api.messages.send(chat_id = 63, message = message(day, minute, hour))
-			time.sleep(1800)
-		elif hour == 11 and minute == 30:
-			api.messages.send(chat_id = 63, message = text_final())
-			return SystemExit
+		(day, minute, hour) = (int(str(datetime.now())[8:10]), int(str(datetime.now())[14:16]),
+							   int(str(datetime.now())[11:13]))
+		
+		print(day, hour, minute)
+
+		if day == 25:
+			api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
+			time.sleep(3 * 3600)
+			return main()
+		elif day == 27:
+			api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
+			time.sleep(2 * 3600)
+			return main()
+		elif day == 28:
+			if 11 > hour >= 9:
+				api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
+				time.sleep(1800)
+				return main()
+			if hour == 11:
+				api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
+				time.sleep(2400)
+				api.messages.send(chat_id = 63, message = text_final())
+				return SystemExit
+			else:
+				api.messages.send(chat_id = 63, message = message(day, minute, hour) + text())
+				time.sleep(3600)
+				return main()
 		else:
-			time.sleep(3600)
-		return main()
+			return main()
 
 
 if __name__ == "__main__":
